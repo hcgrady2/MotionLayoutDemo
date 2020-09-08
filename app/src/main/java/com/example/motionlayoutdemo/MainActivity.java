2211362d67ddf84c221a21d1bdfb2fb54aa6e209
wanghaichao <hcwang1024@163.com>
2020-09-08 22:01:42 +0800

    init

---
+++ app/src/main/java/com/example/motionlayoutdemo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.motionlayoutdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void demoOne(View view) {
+        startActivity(new Intent(MainActivity.this,DemoOne.class));
+    }
+
+    public void imageDemo(View view) {
+        startActivity(new Intent(MainActivity.this,ImageDemo.class));
+    }
+}
